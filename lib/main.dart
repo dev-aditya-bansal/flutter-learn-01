09f9224f5c89c44001761d7817b10462af1125ae
Aditya Bansal <aditya.bansal0712@gmail.com>
2025-08-10 23:37:03 +0530

    2nd version with image url

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,11 @@ void main() {
           // toolbarTextStyle: Colors.white,
           title: Text("My Dashboard App"),
         ),
-        body: Center(child: Image.asset('images/dash.png')),
+        body: Center(
+          child: Image.network(
+            'https://fastly.picsum.photos/id/58/1280/853.jpg?hmac=YO3QnOm9TpyM5DqsJjoM4CHg8oIq4cMWLpd9ALoP908',
+          ),
+        ),
       ),
     ),
   );
